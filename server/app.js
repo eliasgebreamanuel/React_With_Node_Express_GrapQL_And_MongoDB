c059f9beb83f9eb3703b1d51b3c639359380ea08
eliasgebreamanuel <eliasgamanuel@gmail.com>
2023-09-08 21:35:14 +0300

    second commit

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const graphqlHTTP = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema.js');
 
-const app = exress();
+const app = express();
 app.use('/graphql', graphqlHTTP({
-
+    schema,
+    graphiql: true
 }));
 app.listen(4000, () => {
     console.log('now listening for requests on port 4000');
