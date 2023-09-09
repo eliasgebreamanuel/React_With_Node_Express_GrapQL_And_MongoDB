81d7088df1af519f34b00ca38761585130a5ca9e
eliasgebreamanuel <eliasgamanuel@gmail.com>
2023-09-09 22:14:22 +0300

    Second Commit

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import BookList from './components/BookList';
+function App() {
+  return (
+    <div id="main">
+        <h1>London is Blue.</h1>
+        <BookList />
+    </div>
+  );
+}
+
+export default App;
--- server/app.js
+++ server/app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema.js');
+const mongoose = require('mongoose');
+
+// this is used to connect to the mongodb database
+mongoose.connect('mongodb+srv://eliasgebreamanuelhailu:23687930@gql-react.glejirf.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connection.once('open', () => {
+    console.log('Connected To Database');
+});
 
 const app = express();
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
 }));
+
 app.listen(4000, () => {
     console.log('now listening for requests on port 4000');
 });
